eb8e14f5be88e524e7d316979cf3585c9fc19221
Bala Subramanyam Cheethirala <83236119+chvenb@users.noreply.github.com>
2024-01-20 22:19:10 +0530

    Update sendmail.py
    Added function of sending OTP using boto3 (sns)

--- sendmail.py
+++ sendmail.py
@@ -1,26 +1,81 @@
+import boto3
+import tkinter as tk
+from tkinter import messagebox
+import random
+
+def send_otp():
+    
+
+    aws_access_key_id1 = 'AKIASI6SRCQZSBLDSRW6'
+    aws_secret_access_key1 = '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'
+    region_name1 = 'ap-south-1'
+
+    sns = boto3.client('sns', aws_access_key_id=aws_access_key_id1, aws_secret_access_key=aws_secret_access_key1, region_name=region_name1)
+
+    # Specify your SNS topic name
+    topic_name = 'ecompy_211'
+
+    # Get or create the topic ARN
+    response = sns.create_topic(Name=topic_name)
+    topic_arn = response['TopicArn']
+
+    otp = ''.join([str(random.randint(0, 9)) for _ in range(6)])
+
+    # Your message with the OTP
+    message = f'Your OTP is: {otp}'
+
+    # Send an SMS
+    response = sns.publish(
+        TopicArn=topic_arn,
+        Message=message,
+        
+    )
+
+    print(f"Message ID: {response.get('MessageId')}")
+
+    if response.get('MessageId'):
+        print(f"OTP Sent: {otp}")
+    else:
+        print(f"Failed to send OTP. Response: {response}")
+
+    
+
+
+def send_report(num):
+
+    
+
+    aws_access_key_id1 = 'AKIASI6SRCQZSBLDSRW6'
+    aws_secret_access_key1 = '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'
+    region_name1 = 'eu-north-1'
+
+    sns = boto3.client('sns', aws_access_key_id=aws_access_key_id1, aws_secret_access_key=aws_secret_access_key1, region_name=region_name1)
+
+    # Specify your SNS topic name
+    topic_name = 'ecompy-211'
+
+    # Get or create the topic ARN
+    response = sns.create_topic(Name=topic_name)
+    topic_arn = response['TopicArn']
+    
+
+    message = f"the following phone number {num} attempted unauthorised access"
+
+    response = sns.publish(
+        TopicArn=topic_arn,
+        Message=message
+    )
+
+    print("reported")
 
 
 
-import smtplib
-from email.mime.text import MIMEText
-
-email_sender = "botkumar125@gmail.com"
-email_pwd = "botjith125"
-email_recipient = "chvenbalasubramanyam@gmail.com"
-
-def send_email():
-
-    subject = "unauthorized access attempt"
-    body = " someone with unregistered number is attempting to access"
-    message = MIMEText(body)
-    message['Subject'] = subject
-    message['From'] = email_sender
-    message['To'] = email_recipient
-
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
-        server.starttls()
-        server.login(email_sender, email_pwd)
-        server.sendmail(email_sender, email_recipient, message.as_string())
 
 
-send_email()
+
+
+
+
+
+
+
